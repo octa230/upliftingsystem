422a20fcadab8678960e15475f045192c35baac4
octa230 <mervynstunner@outlook.com>
2023-10-21 17:09:25 +0300

    invoice

--- frontend/src/components/SaleHistory.js
+++ frontend/src/components/SaleHistory.js
@@ -12,7 +12,7 @@ import { Link } from 'react-router-dom'
 import Table from 'react-bootstrap/esm/Table'
 import Button from 'react-bootstrap/esm/Button'
 import { PDFViewer } from '@react-pdf/renderer'
-
+import Invoice from '../utils/invoice'
 
 function reducer(state, action){
     switch(action.type){
