898c9bcb7833bad541fc114d162a637aa90796e5
octa230 <mervynstunner@outlook.com>
2024-12-16 17:19:33 +0400

    s

--- backend/server.js
+++ backend/server.js
@@ -39,15 +39,18 @@ app.use(ErrorHandler)
 
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
-if(process.env.NODE_ENV === 'production'){
-    /* app.use(express.static(path.join(__dirname, '../frontend/build')));
-    app.get('*', (req, res)=> res.sendFile(
-        path.resolve(__dirname, '../', 'frontend', 'build', 'index.html')
-    )) */
-    res.sendFile(path.resolve(__dirname, '../frontend/build', 'index.html'))
-}else {
-    app.get('/', (req, res)=> res.send('please set to production mode'))
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../frontend/build')));
+
+    // Serve the React app
+    app.get('*', (req, res) => 
+        res.sendFile(path.resolve(__dirname, '../frontend/build', 'index.html'))
+    );
+} else {
+    app.get('/', (req, res) => res.send('Please set to production mode'));
 }
+
 mongoose.connect(process.env.MONGO_URI)
 .then(()=> {
 
